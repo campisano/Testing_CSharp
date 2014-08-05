df30e4e9990f6e804a4445059077a6b7ec10d7f7
Riccardo Campisano <riccardo_campisano@puc-rio.br>
2014-08-05 13:16:33 -0700

    Test_IDataErrorInfo added

---
+++ Test_IDataErrorInfo/Test_IDataErrorInfo/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Test_IDataErrorInfo
+{
+    public class Employee : IDataErrorInfo
+    {
+        public string Name { get; set; }
+
+        public string Position { get; set; }
+
+        public int Salary { get; set; }
+
+        public string Error
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string this[string columnName]
+        {
+            get
+            {
+                string result = null;
+                if (columnName == "Name")
+                {
+                    if (string.IsNullOrEmpty(Name) || Name.Length < 3)
+                        result = "Please enter a Name";
+                }
+                if (columnName == "Position")
+                {
+                    if (string.IsNullOrEmpty(Position) || Name.Length < 3)
+                        result = "Please enter a Position";
+                }
+                if (columnName == "Salary")
+                {
+                    if (Salary <= 1000 || Salary >= 50000)
+                        result = "Please enter a valid salary amount";
+                }
+                return result;
+            }
+        }
+    }
+}
---
+++ Test_IDataErrorInfo/Test_IDataErrorInfo/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Test_IDataErrorInfo
+{
+    public partial class MainWindow : Window, IDataErrorInfo
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+
+        public string Text { get; set; }
+
+        public string Error
+        {
+            get { throw new System.NotImplementedException(); }
+        }
+
+        public string this[string columnName]
+        {
+            get {
+                return (Text == "" ? "Text" : null);
+            }
+        }
+    }
+}
